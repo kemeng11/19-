3c3c44ebf783d3810345f62c95aa3dacbd2c9100
kemeng <ke11meng@163.com>
2019-03-16 22:49:18 +0800

    增加了链表定义
    增加了函数注释，以及链表定义，每个链表节点代表一个路口，存储了该路口的序号与该路口上下左右相连的路的长度最大限速，以及上下左右节点的指针

--- CodeCraft-2019.cpp
+++ CodeCraft-2019.cpp
@@ -7,11 +7,53 @@ using namespace std;
 
 #define VNAME(value) (#value)
 
-bool txtDataRead(string carPath, vector<vector<int> > &carData,int cols){
+
+struct ListNode{
+    int crossNumber;//交叉路口的序号/id
+
+    //左右上下四条相连路的id
+    int LeftRoadId;//左边路的id
+    int RightRoadId;//右边路的id
+    int UpRoadId;//上面路的id
+    int DownRoadId;//下面路的id
+
+    //左右上下四条相连路的长度
+    int LeftRoadLength;
+    int RightRoadLength;
+    int UpRoadLength;
+    int DownRoadLength;
+
+    //左右上下四条相连路的最快速度
+    int LeftRoadMaxSpeed;
+    int RightRoadMaxSpeed;
+    int UpRoadMaxSpeed;
+    int DownRoadMaxSpeed;
+
+    //左右上下四个相连的节点
+    ListNode* LeftCross;
+    ListNode* RightCross;
+    ListNode* UpCross;
+    ListNode* DownCross;
+};
+
+void CreatRoadMap(vector<vector<int> > RoadData,vector<vector<int> > CrossData, ListNode* MapHead){
+
+
+}
+
+
+bool txtDataRead(string carPath, vector<vector<int> > &carData, int cols){
+// 参数carPath为文件的路径，一开始想对每一个car，path、cross，每一个txt都写一个函数读取，但是发现其实是同一个函数
+//cols即为txt文件里的列数，对于car，cross列数为5，对于road列数为7
+//car  #(id,from,to,speed,planTime)
+//cross #(id,roadId,roadId,roadId,roadId)
+//road #(id,length,speed,channel,from,to,isDuplex)
+
 //目前代码总是会多读一行数据不知道为什么,
 //在原先循坏停止条件为指针走到文件结束的情况下，会多读一行数据
 //目前先计算行数，再读文件
-    std::cout << "carPath is " << carPath << std::endl;
+
+    std::cout << "Path is " << carPath << std::endl;
 
     ifstream infile;
     infile.open(carPath.data());
@@ -66,6 +108,9 @@ void dataShow(vector<vector<int> > arr){
     }
 }
 
+
+
+
 int main()
 {
     //输入文件的路径，包括车辆，道路，交叉口信息，以及输出文件
@@ -88,6 +133,12 @@ int main()
     bool crossDataReadStatus = txtDataRead( crossPath, crossData,5);
     dataShow(carData);
     //将数据转换成图和节点矩阵，双向图，有环图
+
+    vector<int> isVisted;//记录节点是否被访问过，避免在遍历图的时候因为环构成死循环。
+    vector<ListNode*> CrossNodesVector;// 用于存储每一个节点的指针，用于寻找路径，也可能其实用不着，
+                                    //根据路口的序号就可以判断是不是到达目的地
+
+
 
 
     //根据图寻找可行路径，Dijstra找最短路径。
